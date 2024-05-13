b450b7b400742306ab229ca9af259bd918969775
MandarVaidya17 <mandarvaidya1711@gmail.com>
2024-05-13 17:33:53 +0530

    Add project files.

---
+++ WebApplicationProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationProject.Models;
+
+namespace WebApplicationProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["message"] = "Welcome to asp.net core using mvc";
+            List<string> cities = new List<string>()
+            {
+                "Pune","mumbai","Nashik","akole"
+            };
+            ViewData["cities"]=cities;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
